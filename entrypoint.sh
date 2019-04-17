3b388e3711216e0757fce698a2aca2818f38f775
Troy Harvey <troy.harvey@carta.com>
2019-04-17 17:08:00 -0400

    Remove double quotes from the rancher host id output by jq

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ while :
 do
     echo "Checking for inactive hosts every $INTERVAL seconds..."
 
-    rancher hosts --all --format json | jq '{ID,State} | select(.State!="active") | .ID' | xargs -I{} rancher rm --type host {}
+    rancher hosts --all --format json | jq '{ID,State} | select(.State!="active") | .ID' --raw-output | xargs -I{} rancher rm --type host {}
 
     sleep $INTERVAL
 done
