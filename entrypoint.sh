3f3ed1de4ba1662d8eec10e572aa4dfde89e129d
Troy Harvey <troy.harvey@carta.com>
2019-04-19 15:07:17 -0400

    Stop disconnected hosts

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,11 @@ while :
 do
     echo "Checking for inactive hosts every $INTERVAL seconds..."
 
-    rancher hosts --all --format json | jq '{ID,State} | select(.State!="active") | .ID' --raw-output | xargs -I{} rancher rm --type host {}
+    # Stop any disconnected hosts
+    rancher hosts --all --format json | jq '{ID,State} | select(.State=="disconnected") | .ID' --raw-output | xargs -I{} rancher stop {}
+
+    # Remove any inactive hosts
+    rancher hosts --all --format json | jq '{ID,State} | select(.State!="active") | .ID' --raw-output | xargs -I{} rancher rm --type host {} |
 
     sleep $INTERVAL
 done
